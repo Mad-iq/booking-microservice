9426a09ad30560ffa9b555726a531e68c89f713e
Mad-iq <debashritamandal852@gmail.com>
2025-12-01 13:36:36 +0530

    Made changes to the rabbitMQ config

--- src/main/java/com/booking/config/RabbitMQConfig.java
+++ src/main/java/com/booking/config/RabbitMQConfig.java
@@ -1,15 +1,32 @@
 package com.booking.config;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-    public static final String EMAIL_QUEUE = "booking.email.queue";
+
+    public static final String EXCHANGE = "booking.exchange";
+    public static final String ROUTING_KEY = "booking.confirmed";
 
     @Bean
-    public Queue emailQueue() {
-        return new Queue(EMAIL_QUEUE, true);
+    public TopicExchange bookingExchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jsonConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonConverter());
+        return template;
     }
 }
