03af13052d4b3c923ebca2ae781b07bc06d4e3b2
Mad-iq <debashritamandal852@gmail.com>
2025-12-01 12:41:45 +0530

    Added the event layer and emailPayload

---
+++ src/main/java/com/booking/events/EmailPayload.java
@@ -0,0 +1,16 @@
+package com.booking.events;
+
+import java.io.Serializable;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailPayload implements Serializable {
+    private String email;
+    private String name;
+    private String pnr;
+    private String flightId;
+}
